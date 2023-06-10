827bcdf2a567b8ca43eced4e3479951973a0a4bb
01000110 <veereswarg@gmail.com>
2023-06-10 11:52:05 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function previewImage(imageName) {
+function previewImage(imageUrl) {
     var previewModal = document.getElementById('preview-modal');
     var previewImage = document.getElementById('preview-image');
     var downloadLink = document.getElementById('download-link');
 
-    previewImage.src = imageName;
-    downloadLink.href = imageName;
+    previewImage.src = imageUrl;
+    downloadLink.href = imageUrl;
 
     previewModal.style.display = 'block';
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var previewModal = document.getElementById('preview-modal');
+
+    previewModal.addEventListener('click', function() {
+        previewModal.style.display = 'none';
+    });
+});
